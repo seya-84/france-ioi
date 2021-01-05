8c86d12cc05adc15b4facfa76b7c9542b3807fd5
Yannick Severo <yannicksevero@gmail.com>
2021-01-05 11:23:03 +0100

    Ajout des solutions

---
+++ niveau-1/02-Repetitions d'instructions/07-Jeu de dames.py
@@ -0,0 +1,7 @@
+for loop in range(20):
+   for loop in range(20):
+     print("O" "X", end = "")
+   print("")
+   for loop in range(20):
+     print("X" "O", end = "")
+   print("")
---
+++ niveau-1/02-Repetitions d'instructions/10-Le Grand Événement.py
@@ -0,0 +1,20 @@
+from robot import *
+
+droite()
+for loop in range(4):   
+   for loop in range(8):
+      droite()
+   haut()
+   for loop in range(8):
+      gauche()
+   haut()
+   
+for loop in range(8):
+   droite()
+haut()   
+
+for loop in range(9):
+   gauche()
+
+for loop in range(9):
+   bas()
