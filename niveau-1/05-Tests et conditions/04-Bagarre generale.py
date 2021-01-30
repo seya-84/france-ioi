821d44b7658a7cc7588dd85c49425a3094b7b4a0
Yannick Severo <yannicksevero@gmail.com>
2021-01-30 10:10:00 +0100

    Ajout des solutions

---
+++ niveau-1/05-Tests et conditions/04-Bagarre generale.py
@@ -0,0 +1,6 @@
+superficieArignon = int(input())
+superficieEvaran = int(input())
+if superficieArignon > superficieEvaran + 10:
+   print("La famille Arignon a un champ trop grand")
+if superficieEvaran > superficieArignon + 10:
+   print("La famille Evaran a un champ trop grand")
